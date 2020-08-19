d48c4f99f40e7e7fda3125713a22b97d14070ea4
jonghyunlim <dlsrks1218@daum.net>
2020-08-19 14:58:38 +0900

    코드 추가

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-  host: '[수강생의 RDS 주소]',
-  user: '[수강생의 RDS user]',
-  password: '[수강생의 RDS password]',
-  database: '[수강생의 RDS DB]'
+  host: 'rds-project.c42lbtwh1kj8.us-east-1.rds.amazonaws.com',
+  user: 'root',
+  // password: '',
+  database: 'mydb'
 }
