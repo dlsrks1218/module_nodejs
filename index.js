fe9f46e158d2c1484474fe8ce4f5ad9fe1ca03b3
jonghyunlim <dlsrks1218@daum.net>
2020-08-20 01:41:31 +0900

    api 변경

--- index.js
+++ index.js
@@ -149,9 +149,9 @@ app.get('/available_car_list', (req, res) => {
 
 // update문을 수행할 POST 메소드
 app.post('/update_car_data/', (req, res) => {
-  connection.query('update Car set c_no = \'' + Number(req.body.c_no) + '\'where car_no = \'' + Number(req.body.car_num) + '\')', (error, rows) => {
+  connection.query('update Car set c_no = \'' + Number(req.body.c_no) + '\' where car_no = \'' + Number(req.body.car_num) + '\')', (error, rows) => {
     if (error) throw error;
-    console.log('updated car - result is: ', rows);
+    console.log('updated car data - result is: ', rows);
     res.send(rows);
   });
 });
