60eb1fe304ee8296b1b5d5158109c3eff64897eb
jonghyunlim <dlsrks1218@daum.net>
2020-08-19 23:13:13 +0900

    api 변경

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get('/account/pw/:pw', (req, res) => {
 });
 
 // 파라미터 포함 GET method
-app.get('/check_customer_or_salesperson/', (req, res) => {
+app.get('/check_customer_or_salesperson/:idpw', (req, res) => {
   console.log(req.params.id, req.params.pw)
   // connection.query('select * from Salesperson s, Account a where a.id=\'' + req.params.id + '\'and a.pw=\'' + req.params.pw + '\'and s.s_no=a.s_no', (error, rows) => {
   // // connection.query('SELECT * from Account WHERE id=\'' + req.params.pw + '\'', (error, rows) => {
