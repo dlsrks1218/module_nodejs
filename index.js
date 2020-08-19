5b552a509c47928bf6be023e493b6b98b15736fa
jonghyunlim <dlsrks1218@daum.net>
2020-08-20 00:27:31 +0900

    api 변경

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.get('/check_salesperson/', (req, res) => {
 
 // insert문을 수행할 POST 메소드
 app.post('/insert_customer_data/', (req, res) => {
-  console.log(req.query.name)
+  console.log(req.body.name)
   // connection.query('insert into Customer(ticket_cnt, name, age, address, phone, id_no) values(\'' + queryData.ticket_cnt + '\',\'' + queryData.name + '\',\'' + queryData.age + '\',\'' + queryData.address + '\',\'' + queryData.phone + '\',\'' + queryData.phone + '\' , \'' + queryData.id_no + '\')', (error, rows) => {
   // // connection.query('select * from Salesperson s, Account a where a.id=\'' + queryData.id + '\'and a.pw=\'' + queryData.pw + '\'and s.s_no=a.s_no', (error, rows) => {
   //   if (error) throw error;
