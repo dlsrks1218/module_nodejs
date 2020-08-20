750d59f86f395f40b16c66d8e9f615acf2d469d3
jonghyunlim <dlsrks1218@daum.net>
2020-08-20 11:24:52 +0900

    api 변경

--- index.js
+++ index.js
@@ -267,13 +267,13 @@ app.get('/work/:m_no', (req, res) => {
 });
 
 // 파라미터 한개 포함 GET method
-app.get('/repair_or_service/:history', (req, res) => {
-  console.log(req.params.history)
-  // connection.query('select service_no from `Repair-or-Service` where history=\'' + req.params.history + '\'', (error, rows) => {
-  //   if (error) throw error;
-  //   console.log('service number result is: ', rows);
-  //   res.send(rows);
-  // });
+app.get('/repair_or_service/:his', (req, res) => {
+  // console.log(req.params.his)
+  connection.query('select service_no from `Repair-or-Service` where history=\'' + req.params.his + '\'', (error, rows) => {
+    if (error) throw error;
+    console.log('service number result is: ', rows);
+    res.send(rows);
+  });
 });
 
 
