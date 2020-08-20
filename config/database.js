d0f1f3823f6cf8e396f709964b369c64402ed278
jonghyunlim <dlsrks1218@daum.net>
2020-08-20 15:56:36 +0900

    api 변경

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 module.exports = {
-  host: 'database-1.curklx4uwx9b.ap-northeast-2.rds.amazonaws.com',
-  // host: 'project-rds.cqbviwapmnuk.ap-northeast-2.rds.amazonaws.com',
+  // // my rds
+  // host: 'database-1.curklx4uwx9b.ap-northeast-2.rds.amazonaws.com',
+  // proj rds
+  host: 'project-code-deploy-rds.cqbviwapmnuk.ap-northeast-2.rds.amazonaws.com',
   user: 'admin',
   password: 'test1234',
   database: 'mydb'
